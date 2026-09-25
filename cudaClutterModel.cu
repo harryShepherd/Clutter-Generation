996915960f001e261750619cbb3dd0a7c57d22e8
Harry Shepherd <harryshepherd0.0@hotmail.com>
2026-09-25 16:42:18 +0100

    Clutter now generated by cuda

--- cudaClutterModel.cu
+++ cudaClutterModel.cu
@@ -19,32 +19,129 @@
 #include "cudaErr.cuh"
 
 int main()
-{
-    float altitude = 500.0f;
-    float radius_of_earth = 6.37e6f;
-    size_t total_range_rings = 5;
-    std::vector<float> isorange_rings_input = { 1000.0f, 2000.0f, 3000.0f, 4000.0f, 5000.0f };
-    std::vector<float> grazing_angles = { 0.0f, 0.0f, 0.0f, 0.0f, 0.0f };
+{    
+    // parameters
 
-    float* isorange_rings, * grazing_angle;
+    const float transmitted_power = 15000.0f;  // W
+    const float wavelength = 0.03f;            // m
+    const float PRF = 15000.0f;                // Hz
 
-    cudaErrChk(cudaMalloc(&isorange_rings, total_range_rings * sizeof(float)));
-    cudaErrChk(cudaMalloc(&grazing_angle, total_range_rings * sizeof(float)));
+    const float beam_width = 5.0f;             // degs
 
-    cudaErrChk(cudaMemcpy(isorange_rings, isorange_rings_input.data(), total_range_rings * sizeof(float), cudaMemcpyHostToDevice));
-    cudaErrChk(cudaMemcpy(grazing_angle, grazing_angles.data(), total_range_rings * sizeof(float), cudaMemcpyHostToDevice));
+    const int coherent_pulses = 64;
+    const int range_samples = 100;
 
-    GrazingAngle<<<1, 256>>>(altitude, radius_of_earth, total_range_rings, isorange_rings, grazing_angle);
+    const float range_sample_spacing = 100.0f; // m
+
+    const float altitude = 5250.0f;             // m
+    const float ownship_velocity = 500.0f;      // m/s
+
+    const float radius_of_earth = 6.371e6f;     // m
+
+    const float look_down_boresight = 45.0f * CUDART_PI / 180.0f;
+
+    const float look_aside_boresight = 1.0f * CUDART_PI / 180.0f;
+
+    const float delta_azimuth = 0.1f * CUDART_PI / 180.0f;
+
+    float azimuth_min = -5.0f * CUDART_PI / 180.0f; // -5 degs
+    float azimuth_max = 5.0f * CUDART_PI / 180.0f; // +5 degs
+
+    const float gamma = 0.1f;
+
+    const float signal_loss = 1.0f; // 1 means no signal loss
+    
+    int total_isorange_rings = 200;
+
+    std::vector<float> isorange_rings(total_isorange_rings), patches(total_isorange_rings), grazing_angles(total_isorange_rings), look_down_angles(total_isorange_rings);
+    float* dev_isorange_rings, * dev_patches, * dev_grazing_angles, * dev_look_down_angles;
+
+    for (int i = 0; i < total_isorange_rings; ++i)
+    {
+        isorange_rings[i] = i * range_sample_spacing;
+    }
+
+    size_t total_az_patches = static_cast<size_t>((azimuth_max - azimuth_min) / delta_azimuth);
+    size_t total_signals = (total_isorange_rings * total_az_patches * coherent_pulses);
+
+    std::vector<cuFloatComplex> signals(total_signals);
+    cuFloatComplex* dev_signals;
+
+    // end of parameters
+
+    // allocate memory on device
+    cudaErrChk(cudaMalloc(&dev_isorange_rings, total_isorange_rings * sizeof(float)));
+    cudaErrChk(cudaMalloc(&dev_patches, total_isorange_rings * sizeof(float)));
+    cudaErrChk(cudaMalloc(&dev_grazing_angles, total_isorange_rings * sizeof(float)));
+    cudaErrChk(cudaMalloc(&dev_look_down_angles, total_isorange_rings * sizeof(float)));
+    cudaErrChk(cudaMalloc(&dev_signals, total_signals * sizeof(cuFloatComplex)));
+
+    // copy isorange rings to device
+    cudaErrChk(cudaMemcpy(dev_isorange_rings, isorange_rings.data(), total_isorange_rings * sizeof(float), cudaMemcpyHostToDevice));
+
+    // calculate clutter patch areas
+    size_t threads = 124;
+    size_t blocks = std::ceilf(static_cast<float>(total_isorange_rings) / static_cast<float>(threads));
+
+    calculatePatchArea<<<blocks, threads>>>(altitude, radius_of_earth, delta_azimuth, total_isorange_rings, dev_isorange_rings, dev_patches);
+
+    // calculate grazing angles
+    GrazingAngle<<<blocks, threads>>>(altitude, radius_of_earth, total_isorange_rings, dev_isorange_rings, dev_grazing_angles);
+
+    // calculate look down angles
+    LookDownAngle<<<blocks, threads>>>(altitude, radius_of_earth, total_isorange_rings, dev_isorange_rings, dev_look_down_angles);
+
+    // calculate how many signals we need to generate
+    blocks = std::ceilf(static_cast<float>(total_signals) / static_cast<float>(threads));
+
+    CalculateSignal<<<blocks, threads>>>(
+        total_isorange_rings,
+        total_az_patches,
+        coherent_pulses,
+        azimuth_min,
+        delta_azimuth,
+        PRF,
+        ownship_velocity,
+        gamma,
+        beam_width,
+        transmitted_power,
+        wavelength,
+        signal_loss,
+        dev_isorange_rings,
+        dev_patches,
+        dev_look_down_angles,
+        dev_grazing_angles,
+        dev_signals);
 
     cudaErrChk(cudaPeekAtLastError());
-    cudaErrChk(cudaDeviceSynchronize());
+    //cudaErrChk(cudaDeviceSynchronize());
 
-    cudaErrChk(cudaMemcpy(grazing_angles.data(), grazing_angle, total_range_rings * sizeof(float), cudaMemcpyDeviceToHost));
+    cudaErrChk(cudaMemcpy(signals.data(), dev_signals, total_signals * sizeof(cuFloatComplex), cudaMemcpyDeviceToHost));
 
-	std::cout << "Grazing Angle" << std::endl;
+    cudaErrChk(cudaFree(dev_isorange_rings));
+    cudaErrChk(cudaFree(dev_patches));
+    cudaErrChk(cudaFree(dev_grazing_angles));
+    cudaErrChk(cudaFree(dev_look_down_angles));
+    cudaErrChk(cudaFree(dev_signals));
 
-    for (float g : grazing_angles)
+    std::cout << "Complete" << std::endl;
+
+    std::cout << "Writing to file" << std::endl;
+
+    std::ofstream output_file("clutter_signals.txt");
+
+    if (!output_file)
     {
-        std::cout << g << std::endl;
+        std::cerr << "Failed to open output file.\n";
+        return 1;
     }
+
+    for (cuFloatComplex c : signals)
+    {
+        output_file << c.x << " " << c.y << std::endl;
+    }
+
+    std::cout << "Writing Complete" << std::endl;
+
+    output_file.close();
 }
